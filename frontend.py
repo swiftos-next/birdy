87db390b1e39743f00ce2117c1aef7f9e3c5dc85
Natesworks <contact@natesworks.com>
2024-05-30 15:26:20 +0200

    Minor code cleanup

--- frontend.py
+++ frontend.py
@@ -4,7 +4,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--fetch", nargs=2, help="Fetch a package")
-parser.add_argument("--publish",nargs=4, help="Publish a package")
+parser.add_argument("--publish", nargs=4, help="Publish a package")
 
 args = parser.parse_args()
 
@@ -13,7 +13,7 @@ def publish():
     name = args.publish[1]
     version = args.publish[2]
     description = args.publish[3]
-        
+
     module_data = {
         'name': name,
         # placeholder will later use authentication
@@ -34,12 +34,15 @@ def publish():
         exit(1)
 
     files = {'file': (name, content)}
-    response = requests.post('http://localhost:5000/publish', files=files, data=data)
+    response = requests.post(
+        'http://localhost:5000/publish', files=files, data=data)
 
     print(response.text)
 
-if(args.publish):
+
+if (args.publish):
     publish()
+
 
 def download_file(url, destination):
     response = requests.get(url)
@@ -49,6 +52,7 @@ def download_file(url, destination):
         print(f"File downloaded successfully to {destination}")
     else:
         print(f"\033[31mFailed to download file from {url}")
+
 
 def fetch_latest_version(name):
     try:
@@ -63,9 +67,9 @@ def fetch_latest_version(name):
     return None
 
 
-if(args.fetch):
+if (args.fetch):
     name = args.fetch[0]
     output = args.fetch[1]
-    if("-" not in name):
+    if ("-" not in name):
         name = f"{name}-{fetch_latest_version(name)}"
     download_file(f"http://localhost:5000/modules/{name}", output)
